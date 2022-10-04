7bff5a76a46cadbaf01894284d81f00e7c14ad39
Manik Gaur <gaur.manik.mg@gmail.com>
2022-10-04 15:36:59 +0530

    m

---
+++ file.js
@@ -0,0 +1,57 @@
+//Arithmatic Operators.
+let a=20;
+let b=10;
+let b2=30;
+let c= a+b;
+let d= a-b;
+let e= a*b;
+let f= a/b;
+let g= a%b;
+console.log(c,d,e,f,g);
+
+//Relational operators.
+let rel1= a>b;
+let rel2= b<b2;
+let rel3= b>=a;
+let rel4= b==b2;
+let rel5= b!=a;
+let rel6= b2>b;
+console.log(rel1,rel2,rel3,rel4,rel5,rel6);
+
+
+//Logical Operators.
+
+let and= a>b && b2>b;
+let or= a>b || b2>a;
+
+console.log(and,or)
+//conditional operators.
+let cond= a>b2 ? 100 : 200;
+let cond2= b2>b ? 100 : 200;
+console.log(cond,cond2);
+
+//Increment-Decrement Operators.
+let x= --a;
+let y = ++b;
+let z = b2++;
+console.log(x,y,z);
+
+//Assignment Operator.
+let ass1= a+=b;
+let ass2= a-=b;
+let ass3= b*=b2;
+let ass4= b2/=a;
+let ass5= b2%=b;
+console.log(ass1,ass2,ass3,ass4,ass5); 
+
+//Bit wise Operator.
+let bit1= a&b;
+let bit2= a|b;
+let bit3= a>>b;
+let bit4= b>>>a;
+let bit5= b2<<a;
+let bit6= a^b2;
+console.log(bit1,bit2,bit3,bit4,bit5,bit6);
+
+
+
